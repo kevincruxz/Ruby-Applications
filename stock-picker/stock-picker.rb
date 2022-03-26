e10a4ccad7b4443ff8028e06dcecb7e01db87d65
Kevin Cruz <kevin_cruzz@outlook.es>
2022-03-25 21:46:18 -0600

    Finished new app

--- stock-picker/stock-picker.rb
+++ stock-picker/stock-picker.rb
@@ -1,16 +1,18 @@
 def stock_picker(stocks)
     greatest_revenue = 0
+    days_to_commerce = []
     for i in (0..stocks.length - 2) do
         for j in (i + 1..stocks.length - 1) do
             if stocks[j] - stocks[i] > greatest_revenue
                 greatest_revenue = stocks[j] - stocks[i]
+                days_to_commerce = [i, j]
             end
         end
     end
-    greatest_revenue
+    days_to_commerce #Returns the best day to buy, and the best day to sell, in order to get the greatest revenue
 end
 
 print "Write the stocks separeted by a space: "
-stocks = gets.chomp.split
+stocks = gets.chomp.split #Splits the input into an array
 
-puts stock_picker(stocks.map { |int| int.to_i })
+p stock_picker(stocks.map { |int| int.to_i }) #Map method returns the same array converted to integers
